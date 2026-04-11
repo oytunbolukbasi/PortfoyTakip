e7bfa3c931c3aef0810c654415be9ebc7836fc1c
Oytun Bolukbasi <oytunbolukbasi@macbookpro.local>
2026-04-11 19:19:42 +0300

    Remove sensitive and temporary files from repository

--- test-price.ts
+++
@@ -1,6 +0,0 @@
-import { PriceService } from './server/services/price-service.ts';
-async function test() {
-  const s = new PriceService();
-  console.log("USDTRY:", await s.getExchangeRate('USDTRY'));
-}
-test();
